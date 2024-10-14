4ba6cac9168a203c7a705d99ea9a4772010bc286
Liseth <lissiibarra21@gmail.com>
2024-10-13 20:28:43 -0600

    mi aporte es esto de la cafe

---
+++ src/pract/Pract.java
@@ -0,0 +1,19 @@
+
+package pract;
+
+
+public class Pract {
+
+    
+    public static void main(String[] args) {
+        int  [] arco=new int [4];
+       for(int i=4;i<=arco.length && i>0;i--){
+           System.out.println(i);
+       }
+        }
+       
+    }
+    
+
+    
+
